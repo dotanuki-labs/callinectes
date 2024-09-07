4fad8bd6986b8262b34ae7b0df6d42b1957d1d5c
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-09-07 18:33:18 +0200

    Testing another config

--- cargo-plugins.sh
+++ cargo-plugins.sh
@@ -13,24 +13,20 @@ rustup component add rustfmt
 rustup component add clippy
 
 echo
-echo "🦀 Installing cargo-binstall"
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-
-echo
 echo "🦀 Installing cargo-deny"
-cargo binstall cargo-deny@0.16.1 --force --quiet --locked --no-confirm
+cargo install cargo-deny@0.16.1 --force --quiet --locked
 
 echo
 echo "🦀 Installing cargo-msrv"
-cargo binstall cargo-msrv@0.15.1 --force --quiet --locked --no-confirm
+cargo install cargo-msrv@0.15.1 --force --quiet --locked
 
 echo
 echo "🦀 Installing cargo-machete"
-cargo binstall cargo-machete@0.6.2 --force --quiet --locked --no-confirm
+cargo install cargo-machete@0.6.2 --force --quiet --locked
 
 echo
 echo "🦀 Installing cargo-cyclonedx"
-cargo binstall cargo-cyclonedx@0.5.5 --force --quiet --locked --no-confirm
+cargo install cargo-cyclonedx@0.5.5 --force --quiet --locked
 
 echo
 echo "✅ Done"
